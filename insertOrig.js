26f6b1b3d622808496fa28da297f8a1cef341246
Gareth Murphy <garethcmurphy@gmail.com>
2020-03-04 13:03:18 +0100

    add orig

---
+++ insertOrig.js
@@ -0,0 +1,18 @@
+db.OrigDatablock.insertOne({
+  size: 526848,
+  dataFileList: [
+    {
+      path: "/nfs/groups/beamlines/wsafe/WSAFE.hdf5",
+      size: 526848,
+      time: "2019-12-11T12:48:03Z",
+      chk: "34782",
+      uid: "101",
+      gid: "101",
+      perm: "755"
+    }
+  ],
+  ownerGroup: "ess",
+  accessGroups: ["ess", "odin", "loki", "nmx"],
+  createdBy: "ingestor",
+  datasetId: "20.500.12269/beam_test17"
+});
